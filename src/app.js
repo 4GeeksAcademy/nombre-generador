88c4903606fe9c49d3995f8bce4e36776fe7f321
sarah-irc <sarahcardoso.sc@gmail.com>
2025-10-31 10:50:56 +0000

    generador done

--- src/app.js
+++ src/app.js
@@ -9,3 +9,20 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+////// my code from here
+
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let domains = ['com', 'es', 'dev', 'io', 'net'];
+
+  for(let i = 0; i < pronoun.length; i++) {
+    for(let j =0; j < adj.length; j++) {
+      for(let n = 0; n < noun.length; n++) {
+        for(let d = 0; d < domains.length; d++) {
+          console.log(pronoun[1] + adj[j] + noun[n] + '.' + domains[d]);
+        }
+      }
+    }
+  }

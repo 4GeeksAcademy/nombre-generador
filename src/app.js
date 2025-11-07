1ee2dd933fe381117e37009556068f821670de94
sarah-irc <sarahcardoso.sc@gmail.com>
2025-11-07 11:46:04 +0000

    correccion

--- src/app.js
+++ src/app.js
@@ -18,10 +18,10 @@ window.onload = function() {
   let domains = ['com', 'es', 'dev', 'io', 'net'];
 
   for(let i = 0; i < pronoun.length; i++) {
-    for(let j =0; j < adj.length; j++) {
+    for(let j = 0; j < adj.length; j++) {
       for(let n = 0; n < noun.length; n++) {
         for(let d = 0; d < domains.length; d++) {
-          console.log(pronoun[1] + adj[j] + noun[n] + '.' + domains[d]);
+          console.log(pronoun[i] + adj[j] + noun[n] + '.' + domains[d]);
         }
       }
     }
